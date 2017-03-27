917fe4fb2c0382c320e8fd7f6e60c4660d4cd0e2
Mark <lookandlearn76@yahoo.co.uk>
2017-03-27 22:05:26 +0100

    smooth-scroll and sticky-header completed

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,11 +1,20 @@
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
   constructor() {
     this.siteHeader = $(".site-header");
     this.headerTriggerElement = $('.large-hero__title');
     this.createHeaderWaypoint();
+    this.pageSections = $('.page-section');
+    this.HeaderLinks = $('.primary-nav a');
+    this.createPageSectionWaypoints();
+    this.addSmoothScrolling();
+  }
+
+  addSmoothScrolling() {
+    this.HeaderLinks.smoothScroll();
   }
 
   createHeaderWaypoint() {
@@ -18,9 +27,40 @@ class StickyHeader {
         } else {
           that.siteHeader.removeClass('site-header--dark');
         }
+        offset: '20%'
       }
     });
   }
-}
 
+
+  createPageSectionWaypoints() {
+    var that = this;
+    this.pageSections.each(function(){
+      var currentPageSection = this;
+      new Waypoint({
+        element: currentPageSection,
+        handler: function(direction) {
+          if (direction == "down") {
+          var sameHeadLink = currentPageSection.getAttribute('data-matching-link');
+          that.HeaderLinks.removeClass('is-current-link');
+          $(sameHeadLink).addClass('is-current-link');
+          }
+        },
+        offset:"40%",
+      });
+
+      new Waypoint({
+        element: currentPageSection,
+        handler: function(direction) {
+          if (direction == "up") {
+          var sameHeadLink = currentPageSection.getAttribute('data-matching-link');
+          that.HeaderLinks.removeClass('is-current-link');
+          $(sameHeadLink).addClass('is-current-link');
+          }
+        },
+        offset:"-40%",
+      });
+    });
+  }
+}
 export default StickyHeader;
